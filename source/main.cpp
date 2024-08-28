8870bab455f7f0592dc950b00dda591b4b787802
Juan de la Cruz Caravaca Guerrero <juan.dlcruzcg@gmail.com>
2024-08-28 21:43:48 +0200

    Separated stuff into different functions

--- source/main.cpp
+++ source/main.cpp
@@ -7,12 +7,15 @@
 #include <gctypes.h>
 #include <gcutil.h>
 #include <ogc/isfs.h>
+#include <malloc.h>
+#include <ogc/conf.h>
 
 static void* SpXfb = 0;
 static GXRModeObj* SpGXRmode = 0;
 
 
-u16 FindItem(s32 iFileDescriptor);
+u16 LoadOffsets(s32 iFileDescriptor, u16** paurItemOffsets);
+u16 FindItem(s32 iFileDescriptor, u16* paurItemOffsets, u16 urItemCount, const std::string& CsItemName);
 
 
 //---------------------------------------------------------------------------------
@@ -59,13 +62,13 @@ int main(int argc, char **argv)
 
 	s32 iFileDescriptor{-1};
 	s32 iError{ISFS_OK};
-	u16 urOffset ATTRIBUTE_ALIGN(0x20) {};
-	char acItemName[6] ATTRIBUTE_ALIGN(0x20) {};
-	u8 uyNextAspectRatio ATTRIBUTE_ALIGN(0x20) {};
+	u16 urOffset ATTRIBUTE_ALIGN(32) {};
+	char acItemName[6] ATTRIBUTE_ALIGN(32) {};
+	u8 uyAspectRatio ATTRIBUTE_ALIGN(32) {};
 
 	try
 	{
-		if ((iFileDescriptor = ISFS_Open("/shared2/sys/SYSCONF", ISFS_OPEN_READ)) < 0)
+		if ((iFileDescriptor = ISFS_Open("/shared2/sys/SYSCONF", ISFS_OPEN_RW)) < 0)
 			throw std::ios_base::failure(std::string{"Error opening SYSCONF, ret = "} + 
 				std::to_string(iFileDescriptor));
 
@@ -99,33 +102,45 @@ int main(int argc, char **argv)
 					std::to_string(iError));
 
 			if (!strncmp(reinterpret_cast<char*>(&acItemName), "IPL.AR", 6)) urOffset += 7;
-			else urOffset = FindItem(iFileDescriptor);
+			else 
+			{
+				u16* paurItemOffsets{nullptr};
+				u16 urItemCount{LoadOffsets(iFileDescriptor, &paurItemOffsets)};
+				urOffset = FindItem(iFileDescriptor, paurItemOffsets, urItemCount, "IPL.AR");
+				delete[] paurItemOffsets;
+			}
 		}
-		else urOffset = FindItem(iFileDescriptor);
+		else
+		{
+			u16* paurItemOffsets{nullptr};
+			u16 urItemCount{LoadOffsets(iFileDescriptor, &paurItemOffsets)};
+			urOffset = FindItem(iFileDescriptor, paurItemOffsets, urItemCount, "IPL.AR");
+			delete[] paurItemOffsets;
+		}
 
-		if ((iError = ISFS_Read(iFileDescriptor, &uyNextAspectRatio, 1)) < 0)
+		if ((iError = ISFS_Read(iFileDescriptor, &uyAspectRatio, 1)) < 0)
 			throw std::ios_base::failure(std::string{"Error reading item name, ret = "} + 
+				std::to_string(iError));
+
+		uyAspectRatio = (uyAspectRatio == CONF_ASPECT_4_3 ? CONF_ASPECT_16_9 : CONF_ASPECT_4_3);
+
+		// Toggle value
+		if ((iError = ISFS_Seek(iFileDescriptor, -1, 1)) < 0)
+			throw std::ios_base::failure(std::string{"Error seeking SYSCONF, ret = "} + 
+				std::to_string(iError));
+
+		if ((iError = ISFS_Write(iFileDescriptor, &uyAspectRatio, 1)) < 0)
+			throw std::ios_base::failure(std::string{"Error writing to SYSCONF, ret = "} + 
 				std::to_string(iError));
 	}
 	catch (const std::ios_base::failure& CiosBaseFailure)
 	{
-		std::printf("%s\n", CiosBaseFailure.what());
+		std::printf("%s\nPress A to try again.\nPress HOME to exit", CiosBaseFailure.what());
 	}
 
-	if (iFileDescriptor >= 0) 
-	{
-		ISFS_Close(iFileDescriptor);
-		iFileDescriptor = -1;
-	}
+	if (iFileDescriptor >= 0) ISFS_Close(iFileDescriptor);
 
-	uyNextAspectRatio = (uyNextAspectRatio == CONF_ASPECT_4_3 ? CONF_ASPECT_16_9 : CONF_ASPECT_4_3);
-
-	std::printf("Press A to toggle aspect ratio. Current value: %s\n", 
-		(uyNextAspectRatio == CONF_ASPECT_4_3 ? "16:9" : "4:3"));
-
-	std::printf("Press HOME to exit.");
-
-	while(1) 
+	while(iError < 0) // Give option to retry
 	{
 		// Call WPAD_ScanPads each loop, this reads the latest controller states
 		WPAD_ScanPads();
@@ -149,28 +164,16 @@ int main(int argc, char **argv)
 					throw std::ios_base::failure(std::string{"Error seeking SYSCONF, ret = "} + 
 						std::to_string(iError));
 
-				if ((iError = ISFS_Write(iFileDescriptor, &uyNextAspectRatio, 1)) < 0)
+				if ((iError = ISFS_Write(iFileDescriptor, &uyAspectRatio, 1)) < 0)
 					throw std::ios_base::failure(std::string{"Error writing to SYSCONF, ret = "} + 
 						std::to_string(iError));
 			}
 			catch (const std::ios_base::failure& CiosBaseFailure)
 			{
-				std::printf("%s. Perhaps try again?\n", CiosBaseFailure.what());
+				std::printf("%s\nPress A to try again.\nPress HOME to exit", CiosBaseFailure.what());
 			}
 			
-			if (iFileDescriptor >= 0) 
-			{
-				ISFS_Close(iFileDescriptor);
-				iFileDescriptor = -1;
-			}
-
-			if (iError >= 0) uyNextAspectRatio = (uyNextAspectRatio == CONF_ASPECT_4_3 ? 
-				CONF_ASPECT_16_9 : CONF_ASPECT_4_3);
-			
-			std::printf("Press A to toggle aspect ratio. Current value: %s\n", 
-				(uyNextAspectRatio == CONF_ASPECT_4_3 ? "16:9" : "4:3"));
-			
-			std::printf("Press HOME to exit.");
+			if (iFileDescriptor >= 0) ISFS_Close(iFileDescriptor);
 		}
 
 		// We return to the launcher application via exit
@@ -188,10 +191,10 @@ int main(int argc, char **argv)
 }
 
 
-u16 FindItem(s32 iFileDescriptor)
+u16 LoadOffsets(s32 iFileDescriptor, u16** paurItemOffsets)
 {
 	// Search for item
-	u16 urItemCount ATTRIBUTE_ALIGN(0x20) {};
+	u16 urItemCount ATTRIBUTE_ALIGN(32) {};
 	s32 iError{};
 
 	// Seek number of items
@@ -203,27 +206,35 @@ u16 FindItem(s32 iFileDescriptor)
 		throw std::ios_base::failure(std::string{"Error reading number of items, ret = "} + 
 			std::to_string(iError));
 
-	u16 aurItemOffsets[urItemCount] ATTRIBUTE_ALIGN(0x20) {};
+	*paurItemOffsets = static_cast<u16*>(memalign(32, urItemCount << 1));
 
 	// Read all item offsets
-	if ((iError = ISFS_Read(iFileDescriptor, &aurItemOffsets, urItemCount << 1)) < 0)
+	if ((iError = ISFS_Read(iFileDescriptor, *paurItemOffsets, urItemCount << 1)) < 0)
 		throw std::ios_base::failure(std::string{"Error reading number of items, ret = "} + 
 			std::to_string(iError));
 
-	// Check all offsets
-	char acItemName[6] ATTRIBUTE_ALIGN(0x20) {};
+	return urItemCount;
+}
 
+
+u16 FindItem(s32 iFileDescriptor, u16* paurItemOffsets, u16 urItemCount, const std::string& CsItemName)
+{
+	s32 iError{};
+	char acItemName[CsItemName.length()] ATTRIBUTE_ALIGN(32) {};
+
+	// Check all offsets
 	for (s32 i = 0; i < urItemCount; ++i)
 	{
-		if ((iError = ISFS_Seek(iFileDescriptor, aurItemOffsets[i] + 1, 0)) < 0)
+		if ((iError = ISFS_Seek(iFileDescriptor, paurItemOffsets[i] + 1, 0)) < 0)
 			throw std::ios_base::failure(std::string{"Error seeking SYSCONF, ret = "} + 
 				std::to_string(iError));
 
-		if ((iError = ISFS_Read(iFileDescriptor, &acItemName, 6)) < 0)
+		if ((iError = ISFS_Read(iFileDescriptor, &acItemName, CsItemName.length())) < 0)
 			throw std::ios_base::failure(std::string{"Error reading item name, ret = "} + 
 				std::to_string(iError));
 
-		if (!strncmp(reinterpret_cast<char*>(&acItemName), "IPL.AR", 6)) return aurItemOffsets[i] + 7;
+		if (!strncmp(reinterpret_cast<char*>(&acItemName), CsItemName.c_str(), CsItemName.length())) 
+			return paurItemOffsets[i] + 7;
 	}
 
 	throw std::ios_base::failure("Item not found");
